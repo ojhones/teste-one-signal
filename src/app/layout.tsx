32f028a30251c9d147cbc6e5c3c77f791e1ac9ad
jhonatan lima <jhonatan.vinicius.lima@gmail.com>
2024-07-04 15:18:39 -0300

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-import { Head } from 'next/document';
-
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,9 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
+      <head>
         <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async></script>
-      </Head>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
