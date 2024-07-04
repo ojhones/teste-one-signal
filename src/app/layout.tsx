5b7b7f019f13b66822b9ef5baa8e647647cf42de
jhonatan lima <jhonatan.vinicius.lima@gmail.com>
2024-07-04 15:17:32 -0300

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-import Document, { Head } from 'next/document';
+import { Head } from 'next/document';
 
 
 const inter = Inter({ subsets: ["latin"] });
